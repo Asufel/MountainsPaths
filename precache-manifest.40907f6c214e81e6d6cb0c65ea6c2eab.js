17d46576d515fad1a0903a5124b6752e939a58a6
Paweł Leszko <asufel2345@gmail.com>
2020-11-29 19:23:14 +0100

    Updates

---
+++ precache-manifest.40907f6c214e81e6d6cb0c65ea6c2eab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a645aeb02a6829c8db95c2a51439b008",
+    "url": "/mp-project/index.html"
+  },
+  {
+    "revision": "fe23059e1832644cf364",
+    "url": "/mp-project/static/css/2.d436d498.chunk.css"
+  },
+  {
+    "revision": "67b7c987858c97f31851",
+    "url": "/mp-project/static/css/main.0d9cc3a8.chunk.css"
+  },
+  {
+    "revision": "fe23059e1832644cf364",
+    "url": "/mp-project/static/js/2.d6f80d9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mp-project/static/js/2.d6f80d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b7c987858c97f31851",
+    "url": "/mp-project/static/js/main.6b1d098f.chunk.js"
+  },
+  {
+    "revision": "a75653adca9c3c8faeb6",
+    "url": "/mp-project/static/js/runtime-main.1985631d.js"
+  },
+  {
+    "revision": "c046d27142b18b3f3990d773d251ac7c",
+    "url": "/mp-project/static/media/bled-1899264_1920.c046d271.jpg"
+  },
+  {
+    "revision": "998596e58a233a8caec826d434b54e38",
+    "url": "/mp-project/static/media/dolomites-2897227_640.998596e5.jpg"
+  },
+  {
+    "revision": "0194503e5421ebef29f24dfc6a10eb01",
+    "url": "/mp-project/static/media/mountain-climbing-802099_1920.0194503e.jpg"
+  },
+  {
+    "revision": "073aff41e563784255ca7a983a496ca8",
+    "url": "/mp-project/static/media/mountaineering-2124113_1920.073aff41.jpg"
+  },
+  {
+    "revision": "b4f93855ea8a2c0e9f9746344c43dc6c",
+    "url": "/mp-project/static/media/pexels-andrea-piacquadio-774909.b4f93855.jpg"
+  },
+  {
+    "revision": "0de35442dc04e6ace429708a51f28dfd",
+    "url": "/mp-project/static/media/pexels-flo-maderebner-869258.0de35442.jpg"
+  },
+  {
+    "revision": "33b5901ad6d2bf6aa00c65c36f2be742",
+    "url": "/mp-project/static/media/pexels-pixabay-247416.33b5901a.jpg"
+  },
+  {
+    "revision": "daf6d38d53f3e28eb159ee0943b41983",
+    "url": "/mp-project/static/media/pexels-suliman-sallehi-1822458.daf6d38d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.48401c1696289bca03fd04df46e75d66.js"
+  "/mp-project/precache-manifest.40907f6c214e81e6d6cb0c65ea6c2eab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mp-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
